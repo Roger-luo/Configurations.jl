47ccfcb03c595006cd881041ecf0281eafe83399
Roger-luo <rogerluo.rl18@gmail.com>
2021-01-21 10:16:59 -0500

    add docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,13 @@
-using Documenter
+using Documenter, Configurations
+
+makedocs(;
+    modules = [Configurations],
+    format = Documenter.HTML(prettyurls = !("local" in ARGS)),
+    pages = [
+        "Home" => "index.md",
+    ],
+    repo = "https://github.com/Roger-luo/Configurations.jl",
+    sitename = "Configurations.jl",
+)
+
+deploydocs(; repo = "github.com/Roger-luo/Configurations.jl")
