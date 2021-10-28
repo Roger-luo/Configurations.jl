e9853186b11ce6230071ca29459e0885c4709a4b
Roger-Luo <rogerluo.rl18@gmail.com>
2021-10-28 19:05:56 -0400

    improve test

--- test/test_from_dict.jl
+++ test/test_from_dict.jl
@@ -33,6 +33,11 @@ end
     y::Int = 1
 end
 
+@option "option_type" struct ReflectWithAlias
+    type::Reflect
+    y::Int = 1
+end
+
 @option struct TestUnionType
     maybe_reflect::Maybe{OptionReflectA}
     maybe_alias::Maybe{OptionAliasA}
@@ -44,57 +49,89 @@ end
     maybe_totally_mixed::Maybe{Union{OptionAliasA, OptionAliasB, OptionReflectA, OptionReflectB}}
 end
 
-@testset "test dynamic from_dict" begin
-    opt1 = TestUnionType(
-        maybe_reflect = OptionReflectA(),
-        maybe_alias = OptionAliasA(),
-        union_of_alias = OptionAliasB(),
-        union_of_reflects = OptionReflectC(),
-        maybe_of_alias = OptionAliasB(),
-        maybe_of_reflects = OptionReflectC(),
-        totally_mixed = OptionReflectB(),
-        maybe_totally_mixed = OptionAliasA(),
-    )
-    
-    d = to_dict(opt1)
-    from_dict(TestUnionType, d)
+option = TestUnionType(
+            union_of_alias = OptionAliasB(),
+            union_of_reflects = OptionReflectC(),
+            totally_mixed = OptionReflectB(),
+            maybe_totally_mixed = OptionAliasA(),
+        )
+d = to_dict(option)
 
-    for idx in 1:fieldcount(TestUnionType)
-        field = fieldname(TestUnionType, idx)
-        type = fieldtype(TestUnionType, idx)
-        types = Base.uniontypes(type)
-        if Nothing in types
-            type = Union{filter(x->x!==Nothing, types)...}
-        end
-        @test from_dict_union_type_dynamic(
-            TestUnionType,
-            OptionField(field),
-            type, d[string(field)]) == getfield(opt1, field)
-    end        
+
+@testset "from_dict" begin
+    @testset "test maybe union type" begin
+        d = Dict{String, Any}(
+            "maybe_reflect" => Dict{String, Any}(),
+            "maybe_alias" => Dict{String, Any}(),
+            "union_of_alias" => Dict{String, Any}(
+                "option_b" => Dict{String, Any}(
+                    "x" => 1
+                )
+            ),
+            "union_of_reflects" => Dict{String, Any}(
+                "type" => "Main.TestFromDict.OptionReflectC",
+                "y" => 1
+            ),
+            "maybe_of_alias" => nothing,
+            "maybe_of_reflects" => nothing,
+            "totally_mixed" => Dict{String, Any}(
+                "type" => "Main.TestFromDict.OptionReflectB",
+                "x" => 1
+            ),
+            "maybe_totally_mixed" => Dict{String, Any}(
+                "option_a" => Dict{String, Any}(
+                    "x" => 1,
+                    "y" => 2
+                )
+            )
+        )
+        option = TestUnionType(
+            union_of_alias = OptionAliasB(),
+            union_of_reflects = OptionReflectC(),
+            totally_mixed = OptionReflectB(),
+            maybe_totally_mixed = OptionAliasA(),
+        )
+        @test from_dict(TestUnionType, d) == option
+    end
+
+    @testset "test dynamic union type" for option in [
+        TestUnionType(
+            maybe_reflect = OptionReflectA(),
+            maybe_alias = OptionAliasA(),
+            union_of_alias = OptionAliasB(),
+            union_of_reflects = OptionReflectC(),
+            maybe_of_alias = OptionAliasB(),
+            maybe_of_reflects = OptionReflectC(),
+            totally_mixed = OptionReflectB(),
+            maybe_totally_mixed = OptionAliasA(),
+        ),
+        TestUnionType(
+            maybe_reflect = OptionReflectA(),
+            maybe_alias = OptionAliasA(),
+            union_of_alias = OptionAliasA(),
+            union_of_reflects = OptionReflectB(),
+            maybe_of_alias = OptionAliasA(),
+            maybe_of_reflects = OptionReflectB(),
+            totally_mixed = OptionAliasA(),
+            maybe_totally_mixed = OptionReflectB(),
+        ),
+    ]    
+        d = to_dict(option)
+        @test from_dict(TestUnionType, d) == option
+    
+        for idx in 1:fieldcount(TestUnionType)
+            field = fieldname(TestUnionType, idx)
+            type = fieldtype(TestUnionType, idx)
+            types = Base.uniontypes(type)
+            if Nothing in types
+                type = Union{filter(x->x!==Nothing, types)...}
+            end
+            @test from_dict_union_type_dynamic(
+                TestUnionType,
+                OptionField(field),
+                type, d[string(field)]) == getfield(option, field)
+        end        
+    end
 end
 
 end # TestFromDict
-
-using Test
-using Configurations
-using Configurations: from_dict_union_type_dynamic
-
-
-opt1 = TestFromDict.TestUnionType(
-        maybe_reflect = TestFromDict.OptionReflectA(),
-        maybe_alias = TestFromDict.OptionAliasA(),
-        union_of_alias = TestFromDict.OptionAliasB(),
-        union_of_reflects = TestFromDict.OptionReflectC(),
-        maybe_of_alias = TestFromDict.OptionAliasB(),
-        maybe_of_reflects = TestFromDict.OptionReflectC(),
-        totally_mixed = TestFromDict.OptionReflectB(),
-        maybe_totally_mixed = TestFromDict.OptionAliasA(),
-    )
-    
-    d = to_dict(opt1)
-
-from_dict_union_type_dynamic(
-    TestFromDict.TestUnionType,
-    OptionField(:maybe_alias),
-    Maybe{TestFromDict.OptionAliasA}, d["maybe_alias"]
-)
