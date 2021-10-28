31a22dc768a8a2c2bb6d4c45d0a1eb85b54b956e
Roger-Luo <rogerluo.rl18@gmail.com>
2021-10-28 19:17:51 -0400

    test error case for reflection

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 include("test_basic.jl")
 include("test_from_dict.jl")
+include("test_reflection.jl")
 include("test_issue50.jl")
 include("test_reflected.jl")
 include("test_auto_maybe_default.jl")
---
+++ test/test_reflection.jl
@@ -0,0 +1,25 @@
+module TestReflection
+using Test
+using Configurations
+
+@option struct OptionA{T}
+    x::T = 2
+end
+
+@option struct OptionB
+    x::Int = 2
+end
+
+@testset "field_default" begin
+    @testset "error case" begin
+        @test_throws ErrorException field_default(ComplexF32, :im)
+        @test_throws ErrorException field_default(OptionA, :x)
+        @test_throws ErrorException field_default(OptionB, :y)            
+    end
+end
+
+@testset "type_alias" begin
+    @test_throws ErrorException type_alias(Complex)
+end
+
+end
