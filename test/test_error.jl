6e0f2770f678d67f342f6300865b5bfd1985191c
Roger-Luo <rogerluo.rl18@gmail.com>
2021-11-02 23:39:23 -0400

    improve coverage

--- test/test_error.jl
+++ test/test_error.jl
@@ -13,6 +13,7 @@ Configurations.is_option(::OptionA) = true
 @testset "error" begin
     @test_throws ErrorException from_dict(OptionA, Dict{String, Any}("x" => 1))
     @test_throws ErrorException Configurations.type_alias(Float32)
+    @test_throws ErrorException Configurations.type_alias(OptionA)
     @test_throws ErrorException Configurations.get_type_alias_map(Float32)
 end
 
