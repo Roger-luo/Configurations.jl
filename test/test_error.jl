d523dfb17b48ba7ddb550daa67eb4a25f8bb0c78
Roger-Luo <rogerluo.rl18@gmail.com>
2021-11-02 23:34:28 -0400

    test error case

--- test/test_error.jl
+++ test/test_error.jl
@@ -11,7 +11,9 @@ Configurations.is_option(::Type{OptionA}) = true
 Configurations.is_option(::OptionA) = true
 
 @testset "error" begin
-    @test_throws ErrorException from_dict(OptionA, Dict{String, Any}("x" => 1))    
+    @test_throws ErrorException from_dict(OptionA, Dict{String, Any}("x" => 1))
+    @test_throws ErrorException Configurations.type_alias(Float32)
+    @test_throws ErrorException Configurations.get_type_alias_map(Float32)
 end
 
 end
